b5a4ab0a852566419ab123ab1311c59507b7a5a8
GitHub Action <action@github.com>
2023-10-17 02:32:52 +0000

    Template cleanup

--- src/main/kotlin/com/github/kevinguitar/bandlabandroidintellijplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/kevinguitar/bandlabandroidintellijplugin/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.kevinguitar.bandlabandroidintellijplugin.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.kevinguitar.bandlabandroidintellijplugin.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
