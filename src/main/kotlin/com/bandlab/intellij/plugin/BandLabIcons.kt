9eaece54313a298cf2213d086600843eda4936de
Kevin Chiu <kevin85619@gmail.com>
2023-10-20 11:47:15 +0800

    Add the BandLab icon

---
+++ src/main/kotlin/com/bandlab/intellij/plugin/BandLabIcons.kt
@@ -0,0 +1,8 @@
+package com.bandlab.intellij.plugin
+
+import com.intellij.openapi.util.IconLoader
+import javax.swing.Icon
+
+object BandLabIcons {
+    val logo: Icon = IconLoader.getIcon("/drawable/product-bandlab.svg", BandLabIcons::class.java)
+}
--- src/main/kotlin/com/bandlab/intellij/plugin/module/BandLabModuleType.kt
+++ src/main/kotlin/com/bandlab/intellij/plugin/module/BandLabModuleType.kt
@@ -1,5 +1,6 @@
 package com.bandlab.intellij.plugin.module
 
+import com.bandlab.intellij.plugin.BandLabIcons
 import com.intellij.openapi.module.ModuleType
 import javax.swing.Icon
 
@@ -14,11 +15,11 @@ class BandLabModuleType : ModuleType<BandLabModuleBuilder>("bandlab_module") {
     }
 
     override fun getDescription(): String {
-        return "bla bla"
+        return "Create modules easily with BandLab Android convention."
     }
 
     override fun getNodeIcon(isOpened: Boolean): Icon {
-        return org.jdesktop.swingx.icon.EmptyIcon()
+        return BandLabIcons.logo
     }
 
     @Suppress("CompanionObjectInExtension")
