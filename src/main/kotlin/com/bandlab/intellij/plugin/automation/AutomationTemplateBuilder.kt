f28b4d6ead9f5fdc36c8356194731113b4f68ca1
Kevin Chiu <kevin85619@gmail.com>
2024-12-03 16:04:51 +0800

    Fix AutomationTemplateCreateAction for new IDE version

--- src/main/kotlin/com/bandlab/intellij/plugin/automation/AutomationTemplateBuilder.kt
+++ src/main/kotlin/com/bandlab/intellij/plugin/automation/AutomationTemplateBuilder.kt
@@ -2,13 +2,10 @@ package com.bandlab.intellij.plugin.automation
 
 import com.bandlab.intellij.plugin.automation.AutomationTemplateCreateAction.Companion.ANDROID_TEST_DIR
 import com.bandlab.intellij.plugin.utils.resolvePath
-import com.intellij.ide.actions.CreateFileAction.MkDirs
-import com.intellij.internal.statistic.collectors.fus.fileTypes.FileTypeUsageCounterCollector
-import com.intellij.openapi.application.WriteAction
 import com.intellij.psi.PsiDirectory
-import com.intellij.psi.PsiDocumentManager
 import com.intellij.psi.PsiElement
-import com.intellij.psi.PsiFile
+import com.intellij.psi.PsiFileFactory
+import org.jetbrains.kotlin.idea.KotlinFileType
 
 class AutomationTemplateBuilder(
     private val name: String,
@@ -32,7 +29,7 @@ class AutomationTemplateBuilder(
                 private val semantics = ${name}Semantics(rule)
 
                 fun verify(block: ${name}Verifier.() -> Unit) = apply {
-                    ${name}Verifier(semantics).block()
+                    ${name}Verifier(rule, semantics).block()
                 }
             }
 
@@ -80,15 +77,10 @@ class AutomationTemplateBuilder(
         fileName: String,
         content: String
     ): PsiElement {
-        val mkdirs = MkDirs(name, directory)
-        val filePsi = WriteAction.compute<PsiFile, RuntimeException> {
-            mkdirs.directory.createFile(fileName)
-        }
+        val psiFile = PsiFileFactory
+            .getInstance(directory.project)
+            .createFileFromText(fileName, KotlinFileType.INSTANCE, content)
 
-        val psiDocumentManager = PsiDocumentManager.getInstance(directory.project)
-        psiDocumentManager.getDocument(filePsi)?.setText(content)
-
-        FileTypeUsageCounterCollector.triggerCreate(filePsi.project, filePsi.virtualFile)
-        return filePsi
+        return directory.add(psiFile)
     }
 }
