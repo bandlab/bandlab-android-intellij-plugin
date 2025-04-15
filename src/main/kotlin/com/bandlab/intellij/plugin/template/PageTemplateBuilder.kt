f28e743475edbc9f6c2957bc1dfd32b0917ab881
Kevin Chiu <kevin85619@gmail.com>
2025-04-15 13:35:22 +0800

    Adjust PageTemplateBuilder according to the latest ContributesComponent convention

--- src/main/kotlin/com/bandlab/intellij/plugin/template/PageTemplateBuilder.kt
+++ src/main/kotlin/com/bandlab/intellij/plugin/template/PageTemplateBuilder.kt
@@ -10,22 +10,20 @@ class PageTemplateBuilder(
         import android.content.Context
         import androidx.compose.runtime.Composable
         import com.bandlab.common.android.di.ContributesComponent
-        import com.bandlab.common.android.di.HasServiceProvider
+        import com.bandlab.common.android.pager.screen.di.HasPageServiceProvider
         import com.bandlab.common.android.pager.screen.di.componentCreator
         import com.bandlab.uikit.compose.pager.Page
         import javax.inject.Inject
 
         @ContributesComponent(appDependencies = ${name}Page.ServiceProvider::class)
-        class ${name}Page @Inject constructor(context: Context) : Page<${name}ViewModel>, HasServiceProvider {
+        class ${name}Page @Inject constructor(context: Context) : Page<${name}ViewModel>, HasPageServiceProvider {
 
-            private val component by componentCreator(context, Dagger${name}PageComponent.factory())
+            override val componentCreator = componentCreator(context, Dagger${name}PageComponent.factory())
 
             @Composable
             override fun Content(vm: ${name}ViewModel) {
                 
             }
-
-            override fun <T> resolve(): T = HasServiceProvider.resolveFrom(component)
 
             interface ServiceProvider {
                 
