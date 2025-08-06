c584bb6d98216df60285636a256216e3908e16ed
Kevin Chiu <kevin85619@gmail.com>
2025-08-06 14:32:25 +0800

    Update project setup according to the latest template

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,5 @@
+rootProject.name = "bandlab-android-intellij-plugin"
+
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
-
-rootProject.name = "bandlab-android-intellij-plugin"
