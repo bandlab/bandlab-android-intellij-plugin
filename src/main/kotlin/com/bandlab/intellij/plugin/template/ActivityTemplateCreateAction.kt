1a20b0cef611f3b270d6a0f51d87468bdd4422c2
Kevin Chiu <kevin.chiu@bandlab.com>
2026-02-06 15:00:02 +0800

    Create a CreateSimpleFileAction abstraction

--- src/main/kotlin/com/bandlab/intellij/plugin/template/ActivityTemplateCreateAction.kt
+++ src/main/kotlin/com/bandlab/intellij/plugin/template/ActivityTemplateCreateAction.kt
@@ -1,62 +1,17 @@
 package com.bandlab.intellij.plugin.template
 
-import com.bandlab.intellij.plugin.BandLabIcons
 import com.bandlab.intellij.plugin.utils.filePackage
 import com.bandlab.intellij.plugin.utils.resolvePath
 import com.bandlab.intellij.plugin.utils.writeFile
-import com.intellij.ide.actions.CreateFileAction
-import com.intellij.ide.ui.newItemPopup.NewItemPopupUtil
-import com.intellij.ide.ui.newItemPopup.NewItemSimplePopupPanel
-import com.intellij.openapi.actionSystem.DataContext
-import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiDirectory
 import com.intellij.psi.PsiElement
-import org.jetbrains.kotlin.idea.refactoring.psiElement
-import java.awt.event.InputEvent
-import java.util.function.Consumer
 
-class ActivityTemplateCreateAction : CreateFileAction(
-    { "Activity Template" },
-    { "Create an activity template with latest convention." },
-    { BandLabIcons.logo },
+class ActivityTemplateCreateAction : CreateSimpleFileAction(
+    text = "Activity Template",
+    description = "Create an activity template with latest convention.",
+    inputHint = "Feature Name (Ex: UserLibrary)",
+    availability = Availability.MainOnly
 ) {
-
-    /**
-     * Make the action only available for main source set.
-     */
-    override fun isAvailable(dataContext: DataContext): Boolean {
-        if (!super.isAvailable(dataContext)) return false
-
-        val targetPath = dataContext.psiElement?.resolvePath() ?: return false
-        return targetPath.contains("main/")
-    }
-
-    // There is no way to override the "New File" text from the file creation popup,
-    // so copied the code from the parent and override the popup title.
-    override fun invokeDialog(
-        project: Project,
-        directory: PsiDirectory,
-        elementsConsumer: Consumer<in Array<PsiElement>>
-    ) {
-        val validator = MyValidator(project, directory)
-        val contentPanel = NewItemSimplePopupPanel()
-        val nameField = contentPanel.textField
-        val popup = NewItemPopupUtil.createNewItemPopup("Feature Name (Ex: UserLibrary)", contentPanel, nameField)
-
-        contentPanel.applyAction = com.intellij.util.Consumer { event: InputEvent? ->
-            val name = nameField.text
-            if (validator.checkInput(name) && validator.canClose(name)) {
-                popup.closeOk(event)
-                elementsConsumer.accept(validator.createdElements)
-            } else {
-                val errorMessage = validator.getErrorText(name)
-                contentPanel.setError(errorMessage)
-            }
-        }
-
-        popup.showCenteredInCurrentWindow(project)
-    }
-
     override fun create(newName: String, directory: PsiDirectory): Array<PsiElement> {
         val activityBuilder = ActivityTemplateBuilder(
             name = newName,
