f48853abdec2d155490daa85a3f7f61d6f9c391f
Kevin Chiu <kevin.chiu@bandlab.com>
2026-03-12 16:45:27 +0800

    Allow multiple run tasks

--- src/main/kotlin/com/bandlab/intellij/plugin/analysis/ModuleAnalysisAction.kt
+++ src/main/kotlin/com/bandlab/intellij/plugin/analysis/ModuleAnalysisAction.kt
@@ -3,6 +3,8 @@ package com.bandlab.intellij.plugin.analysis
 import com.bandlab.intellij.plugin.BandLabIcons
 import com.bandlab.intellij.plugin.utils.GradleProjectUtils
 import com.bandlab.intellij.plugin.utils.isAndroidModule
+import com.intellij.execution.ExecutionManager
+import com.intellij.execution.ExecutionTargetManager
 import com.intellij.execution.executors.DefaultRunExecutor
 import com.intellij.openapi.actionSystem.ActionUpdateThread
 import com.intellij.openapi.actionSystem.AnActionEvent
@@ -10,7 +12,7 @@ import com.intellij.openapi.actionSystem.CommonDataKeys
 import com.intellij.openapi.actionSystem.LangDataKeys
 import com.intellij.openapi.externalSystem.model.ProjectSystemId
 import com.intellij.openapi.externalSystem.model.execution.ExternalSystemTaskExecutionSettings
-import com.intellij.openapi.externalSystem.service.execution.ProgressExecutionMode
+import com.intellij.openapi.externalSystem.service.execution.ExternalSystemRunConfiguration
 import com.intellij.openapi.externalSystem.util.ExternalSystemUtil
 import com.intellij.openapi.project.DumbAwareAction
 import com.intellij.openapi.roots.ModuleRootManager
@@ -39,9 +41,11 @@ class ModuleAnalysisAction : DumbAwareAction(
         val gradleProjectFolder = selectedFolder?.takeIf(GradleProjectUtils::isGradleProject) ?: contentRoot
         val gradlePath = GradleProjectUtils.getGradleProjectPath(project, gradleProjectFolder) ?: return
 
+        val taskExecutionName = "Module Analysis ($gradlePath)"
+        val systemId = ProjectSystemId("GRADLE")
         val settings = ExternalSystemTaskExecutionSettings().apply {
-            executionName = "Module Analysis ($gradlePath)"
-            externalSystemIdString = "GRADLE"
+            executionName = taskExecutionName
+            externalSystemIdString = systemId.id
             externalProjectPath = project.basePath
             taskNames = if (project.isAndroidModule(gradleProjectFolder.path)) {
                 listOf("$gradlePath:analyzeModule")
@@ -50,14 +54,26 @@ class ModuleAnalysisAction : DumbAwareAction(
             }
         }
 
-        ExternalSystemUtil.runTask(
-            /* taskSettings = */ settings,
-            /* executorId = */ DefaultRunExecutor.EXECUTOR_ID,
-            /* project = */ project,
-            /* externalSystemId = */ ProjectSystemId.findById("GRADLE") ?: ProjectSystemId.IDE,
-            /* callback = */ null,
-            /* progressExecutionMode = */ ProgressExecutionMode.IN_BACKGROUND_ASYNC,
-            /* activateToolWindowBeforeRun = */ true
+        val runnerSettings = ExternalSystemUtil.createExternalSystemRunnerAndConfigurationSettings(
+            settings,
+            project,
+            systemId
+        ) ?: return
+
+        val runConfig = runnerSettings.configuration
+        if (runConfig is ExternalSystemRunConfiguration) {
+            runConfig.isAllowRunningInParallel = true
+        }
+
+        runnerSettings.name = taskExecutionName
+
+        val target = ExecutionTargetManager.getActiveTarget(project)
+        ExecutionManager.getInstance(project).restartRunProfile(
+            project,
+            DefaultRunExecutor.getRunExecutorInstance(),
+            target,
+            runnerSettings,
+            null
         )
     }
 }
