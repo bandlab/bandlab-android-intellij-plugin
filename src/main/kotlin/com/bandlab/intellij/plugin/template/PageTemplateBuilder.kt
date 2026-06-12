23f09a5366c5e6e0ba910829ec844eacf5f4a505
Kevin Chiu <kevin85619@gmail.com>
2026-06-12 10:25:04 +0800

    Remove `Context` from Page constructor in the template (#219)
    * Remove Context from Page template
    
    * changelog

--- src/main/kotlin/com/bandlab/intellij/plugin/template/PageTemplateBuilder.kt
+++ src/main/kotlin/com/bandlab/intellij/plugin/template/PageTemplateBuilder.kt
@@ -23,18 +23,14 @@ class PageTemplateBuilder(
     fun createPage(): String = """
         package $filePackage
         
-        import android.content.Context
         import androidx.compose.runtime.Composable
-        import com.bandlab.common.android.pager.screen.di.HasPageServiceProvider
-        import com.bandlab.common.android.pager.screen.di.graphCreator
         import com.bandlab.metro.station.MetroStation
         import $pageImport
         import dev.zacsweers.metro.Inject
-        import dev.zacsweers.metro.createGraphFactory       
 
         @MetroStation(appDependencies = ${name}Page.ServiceProvider::class)
         @Inject
-        class ${name}Page(context: Context) : $pageType {
+        class ${name}Page : $pageType {
 
             @Composable
             override fun Content(viewModel: ${name}ViewModel) {
