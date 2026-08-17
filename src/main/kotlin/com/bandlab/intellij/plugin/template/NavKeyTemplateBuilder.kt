b851dd225c86b70e74e98f63493f9e71df96ca9c
Kevin Chiu <kevin.chiu@bandlab.com>
2026-08-17 13:30:31 +0800

    Update Page with NavKey template

--- src/main/kotlin/com/bandlab/intellij/plugin/template/NavKeyTemplateBuilder.kt
+++ src/main/kotlin/com/bandlab/intellij/plugin/template/NavKeyTemplateBuilder.kt
@@ -9,28 +9,21 @@ class NavKeyTemplateBuilder(
         package $filePackage
         
         import com.bandlab.models.navigation.GlobalPageNavKey
+        import com.bandlab.navigation.ui.GlobalPageNavEntry
+        import com.bandlab.uikit.api.page.Page
+        import dev.zacsweers.metro.AppScope
+        import dev.zacsweers.metro.ContributesIntoSet
         import kotlinx.serialization.Serializable
 
         @Serializable
         data class ${name}Key(
             val id: String // TODO: Your params
         ) : GlobalPageNavKey
-    """.trimIndent()
-
-    fun createNavEntry(): String = """
-        package $filePackage
         
-        import androidx.activity.ComponentActivity
-        import com.bandlab.navigation.ui.GlobalPageNavEntry
-        import com.bandlab.uikit.api.page.Page
-        import dev.zacsweers.metro.AppScope
-        import dev.zacsweers.metro.ContributesIntoSet
-        import dev.zacsweers.metro.Inject
-
         @ContributesIntoSet(scope = AppScope::class)
         internal object ${name}NavEntry : GlobalPageNavEntry<${name}Key> {
             override val keyInfo = GlobalPageNavEntry.KeyInfo(${name}Key::class, ${name}Key.serializer())
-            override fun getPage(key: ${name}Key, activity: ComponentActivity): Page<*> = ${name}Page(activity)
+            override fun getPage(key: ${name}Key): Page<*> = ${name}Page()
         }
     """.trimIndent()
 }
